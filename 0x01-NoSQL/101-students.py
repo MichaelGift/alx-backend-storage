d619f24f28f1e1d6875e58b8d1d3c3f521021d2c
Michael Gift <simiyugift@outlook.com>
2024-07-17 04:45:41 +0300

    Print top students with best average score

---
+++ 0x01-NoSQL/101-students.py
@@ -0,0 +1,28 @@
+#!/usr/bin/env python3
+"""Top students by average score
+"""
+
+
+def top_students(mongo_collection):
+    """Print the list of students having the best average score.
+    """
+    students = mongo_collection.aggregate(
+        [
+            {
+                '$project': {
+                    '_id': 1,
+                    'name': 1,
+                    'averageScore': {
+                        '$avg': {
+                            '$avg': '$topics.score',
+                        },
+                    },
+                    'topics': 1,
+                },
+            },
+            {
+                '$sort': {'averageScore': -1},
+            },
+        ]
+    )
+    return students
